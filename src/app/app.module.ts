b60fc517eca3583e0f5ae1c614d035db65e5bd3c
Micha <somme@gmx.de>
2017-12-21 21:00:53 +0100

    Add basic top-navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MatchdayComponent } from './matchday/matchday.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchdayComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/matchday/matchday.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matchday',
+  templateUrl: './matchday.component.html',
+  styleUrls: ['./matchday.component.css']
+})
+export class MatchdayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

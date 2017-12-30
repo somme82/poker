931cd442404b82cb8fbb6d090b2598218e66aea8
Micha <somme@gmx.de>
2017-12-30 17:27:39 +0100

    Try to show matchdayResults by using custom array of playerResults
    does not work yet! The Array is 'not ready' when onInit is done.
    the entries are not shown

--- src/app/scoretable/scoretable.component.ts
+++ src/app/scoretable/scoretable.component.ts
@@ -10,9 +10,13 @@ import {MatNativeDateModule} from '@angular/material';
 import {MatDatepicker} from '@angular/material';
 import {FormControl} from '@angular/forms';
 import {IScore} from '../matchday/matchday.component';
+import {forEach} from '@angular/router/src/utils/collection';
+import 'rxjs/add/observable/of';
 
 export interface IPlayer {
   name: string;
+  totalscore: number;
+  totalbuyin: number;
 }
 
 export interface IMatchday{
@@ -33,14 +37,22 @@ interface IMatchday_meta extends IMatchday{
 export class ScoretableComponent implements OnInit {
 
   playersCollection: AngularFirestoreCollection<IPlayer>;
-  players: Observable<IPlayer[]>;
+  players: any
+
+  scoreCollection: AngularFirestoreCollection<IScore>;
+  scores: any;
+
   matchdayDate: Date;
   newMatchdayDoc: AngularFirestoreDocument<IScore>;
   newMatchday: any;
 
+
+  playerResults: IPlayer[] = new Array<IPlayer>();
+
   @ViewChild(MatDatepicker) datepicker: MatDatepicker<Date>;
 
   constructor(private firestore: AngularFirestore, public dialog: MatDialog, private globalVars: GlobalVars) {
+
   }
 
   onDateChange = (e: MatDatepickerInputEvent<Date>) => {
@@ -55,15 +67,9 @@ export class ScoretableComponent implements OnInit {
     this.globalVars.matchdayId = pushkey;
   }
 
-  dateCtrl = new FormControl();
-
   openDialog() {
-    const dialogRef = this.dialog.open(UserDialogComponent, {
+    this.dialog.open(UserDialogComponent, {
       panelClass: 'fnpc-dialog'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
     });
   }
   openDatepicker()
@@ -76,7 +82,56 @@ export class ScoretableComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.playersCollection = this.firestore.collection('players', ref => ref.orderBy('totalscore', 'desc'));
-    this.players = this.playersCollection.valueChanges();
+    this.playersCollection = this.firestore.collection('players');
+    this.players = this.playersCollection.snapshotChanges()
+      .map(actions => {
+        return actions.map( a => {
+          const data = a.payload.doc.data() as IPlayer;
+          const id = a.payload.doc.id;
+          return {id, data};
+        });
+      });
+  }
+
+  getPlayerResults(){
+      this.playerResults = new Array<IPlayer>()
+      console.log('begin getPlayerResults');
+      this.scoreCollection = this.firestore.collection('scores');
+      this.scores = this.scoreCollection.snapshotChanges()
+        .map(actions => {
+          return actions.map( a => {
+            const data = a.payload.doc.data() as IScore;
+            const id = a.payload.doc.id;
+            return {id, data};
+          });
+        });
+
+      this.scores.subscribe( s => {
+        this.scores = s;
+        if (this.scores && this.scores.length > 0) {
+          this.scores.forEach(s => {
+            if (this.playerResults.some(p => p.name === s.data.player))
+            {
+              this.playerResults.find(p => p.name === s.data.player).totalscore += s.data.totalscore;
+              this.playerResults.find(p => p.name === s.data.player).totalbuyin += s.data.buyin;
+            } else {
+              var player = <IPlayer>{};
+              player.name = s.data.player;
+              player.totalscore = s.data.totalscore;
+              player.totalbuyin = s.data.buyin;
+              this.playerResults.push(player);
+            }
+          })
+        }
+
+        this.playerResults = this.playerResults.sort(function(a, b){
+          return b.totalscore-a.totalscore;
+        });
+        console.log(this.playerResults);
+      });
+  }
+
+  getNextYear()
+  {
   }
 }
